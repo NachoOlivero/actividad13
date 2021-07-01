55372d33b32427795673e24d227645ef5fb89329
Nacho <54416746+NachoOlivero@users.noreply.github.com>
2021-07-01 18:26:55 -0300

    cambios

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
+import java.util.*
+
+
+fun main(args: Array<String>) {
+    println("\nFlujo maximo: ${EdmonsKarp().edmonsKarp()}")
+}
+
 class EdmonsKarp {
 
     private var cantNodos = 0
@@ -10,40 +17,69 @@ class EdmonsKarp {
     private var cf = 0
     private lateinit var padre: IntArray
 
-    fun main(args: Array<String>) {
-        println(edmonsKarp())
-    }
-
     fun edmonsKarp(): Int {
-        // leo los valores n y m
-        // leo s y t
-        // leo los arcos
+        val sc = Scanner(System.`in`)
+
+        cantNodos = sc.nextInt()
+        cantArcos = sc.nextInt()
+
         arcos = Array(cantNodos) { IntArray(cantNodos) }
         flujo = Array(cantNodos) { IntArray(cantNodos) }
 
+        s = sc.nextInt()-1
+        t = sc.nextInt()-1
+
+        for(i in 0 until cantArcos) {
+            val u = sc.nextInt()
+            val v = sc.nextInt()
+            val cap = sc.nextInt()
+            arcos[u-1][v-1] = cap
+        }
+
         while (true) {
-            bfs()
-            if(cf == 0)
+            if(!bfs())
                 break
             var nodo = t
             var nodoPadre: Int
-            while(nodo != s) {
+
+            while (nodo != s) {
                 nodoPadre = padre[nodo]
-                flujo[nodo][nodoPadre] += cf
-                flujo[nodoPadre][nodo] -= cf
+                flujo[nodo][nodoPadre] -= cf
+                flujo[nodoPadre][nodo] += cf
                 nodo = nodoPadre
             }
         }
 
         var maxFlujo = 0
-        for(i in 0 until cantNodos-1)
-            maxFlujo+=flujo[s][i]
+        for (i in 0 until cantNodos)
+            maxFlujo += flujo[s][i]
 
         return maxFlujo
     }
 
-    private fun bfs() {
-        cf = 0
+    private fun bfs(): Boolean {
+        cf = Int.MAX_VALUE
         padre = IntArray(cantNodos)
-        // queda hacer o ver si esta hecho
+
+        var visitado = BooleanArray(cantNodos)
+        var cola: Queue<Int> = ArrayDeque()
+        visitado[s] = true
+
+        cola.add(s)
+        while(!cola.isEmpty()) {
+            var arco = cola.remove()
+            for(i in 0 until cantNodos)
+                if(!visitado[i] && arcos[arco][i] > flujo[arco][i]) {
+                    visitado[i] = true
+                    padre[i] = arco
+                    cf = cf.coerceAtMost(arcos[arco][i] - flujo[arco][i])
+
+                    if (i != t) {
+                        cola.add(i)
+                    }
+                    else return true
+                }
+        }
+        return false
+    }
 }
